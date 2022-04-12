b74d768044e1691677abfeab710d1c1412953b19
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2022-04-12 15:26:52 +0200

    exercice ajax simpson closed

---
+++ app.js
@@ -0,0 +1,20 @@
+function fetchSimpsonJSON() {
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+  axios
+    .get(url)
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (simpson) {
+      console.log("data decoded from JSON:", simpson);
+
+      const simpsonHtml = `
+        <p><strong>${simpson[0].character}</strong></p>
+        <p>${simpson[0].quote}</p>
+        <img src="${simpson[0].image}" />
+      `;
+      document.querySelector("#simpson").innerHTML = simpsonHtml;
+    });
+}
+
+fetchSimpsonJSON();
